71dc3d1b8666e885ffa3f51854293fd22875f0bd
jwieckow <hubmar19>
2019-12-04 20:07:49 +0100

    fix in building application to jar file

--- app/application/src/main/java/com/coordinator/CoordinatorApplication.java
+++ app/application/src/main/java/com/coordinator/CoordinatorApplication.java
@@ -1,9 +1,11 @@
 package com.coordinator;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.coordinator")
+@EnableAutoConfiguration
 public class CoordinatorApplication {
 
     public static void main(String[] args) {
